2bd1f0d56cee7f84ae8def189bfa41a6beb30a9a
omya6av <omya6av@gmail.com>
2025-02-20 11:15:32 +0530

    [Om] Add. solution of filter json

---
+++ Day2-JSON-Handling/src/main/java/com/handsonproblems/filterjson/FilterJSON.java
@@ -0,0 +1,23 @@
+package com.json.practiceproblems.filterjson;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+
+public class FilterJSON {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            File file = new File("C:\\Users\\91989\\Desktop\\Week-5-IO-Programming\\Day2-JSON-Handling\\src\\main\\java\\com\\handsonproblems\\filterjson\\students.json");
+            JsonNode jsonNode = objectMapper.readTree(file);
+            for (JsonNode node : jsonNode) {
+                if (node.has("age") && node.get("age").asInt() > 25) {
+                    System.out.println(node);
+                }
+
+
+        } }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
