49093a253550cd9a1a1236313becead825b82db9
omya6av <omya6av@gmail.com>
2025-02-20 11:19:57 +0530

    [Om] Add. solution of convert to object to array

---
+++ Day2-JSON-Handling/src/main/java/com/jsonhandling/convertobjecttoarray/ObjectToJSONArray.java
@@ -0,0 +1,21 @@
+package com.jsonhandling.convertobjecttoarray;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ObjectToJSONArray {
+    public static void main(String[] args) {
+        JSONObject student = new JSONObject();
+        JSONObject student2 = new JSONObject();
+        JSONObject student3 = new JSONObject();
+        student.put("Name","Naman");
+        student2.put("Name","Ishan");
+        student3.put("Name","Om");
+        JSONArray students = new JSONArray();
+        students.put(student);
+        students.put(student2);
+        students.put(student3);
+
+        System.out.println(students);
+    }
+}
